ded5104d30cf704687c3abd6834736e13f5d3248
= <thang.huynh304@gmail.com>
2020-09-26 14:27:11 +0700

    finish link page and add notfound page

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,30 @@ import * as serviceWorker from './serviceWorker';
 import UIweb from './views/UIweb';
 import Login from './views/login';
 import Register from './views/register';
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import NotFound from './views/notFound';
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <UIweb/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Register/>
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+      <Route path="/" exact component={UIweb} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+      <Route component={NotFound} />
+    </div>
+  </Router>
 );
 
+ReactDOM.render(
+  routing, document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
