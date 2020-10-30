ad73ba1939e0f6bc4f6fd37448788ca2b08a2c08
= <=>
2020-10-30 18:44:31 +0700

    update create

--- client/src/views/Schedule.js
+++ client/src/views/Schedule.js
@@ -2,14 +2,34 @@ import React, { useState } from "react";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+class Schedule extends React.Component {
 
-export default function Schedule() {
-  const [date, setDate] = useState(new Date());
-  console.log(date)
+constructor(props){
+  super(props)
+  this.state={
+    time:''
+  }
+}
+handleTime = (e) => {
+  this.setState({ time: e.target.value })
+}
+render(){
+  // console.log(date);
+  console.log(this.state.time)
+  const[date, setDate] = useState(new Date());
   return (
-    <DatePicker 
-    closeOnScroll={e => e.target.value === document}
-    selected={date} 
-    onChange={date => setDate(date)} />
+    <div>
+      <DatePicker
+        closeOnScroll={e => e.target.value === document}
+        selected={date}
+        onChange={date => setDate(date)} />
+      <div class="md-form md-outline">
+        <input type="time" id="default-picker" class="form-control" placeholder="Select time" onChange={this.handleTime} />
+        <label for="default-picker">Default Time Picker</label>
+      </div>
+
+    </div>
   );
 }
+}
+export default Schedule;
