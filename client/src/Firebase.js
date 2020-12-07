f8bf3ff8dbf4e341723b1b8aece44e1e5c9fea1c
= <thang.huynh304@gmail.com>
2020-12-07 21:55:39 +0700

    send email noti

--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase';
 const settings = { timestampsInSnapshots: true };
+
 const config = {
-    projectId: 'chatdemo-4939f',
+    projectId: 'chatrealtime-28e2c',
     apiKey: 'AIzaSyB-Iu6leWu8YIq5RZNXx_dExEsuDpaW-MQ',
     databaseURL: 'https://chatdemo-4939f.firebaseio.com'
 };
+
+const config1 = {
+    projectId: 'chatdemo-4939f',
+    apiKey: 'AIzaSyAkTXyrX-o8am7_w8NcGfqKtWzDeEm61Ig',
+    databaseURL: 'https://chatrealtime-28e2c.firebaseio.com'
+};
+
 firebase.initializeApp(config);
 firebase.firestore().settings(settings);
 export const auth = firebase.auth;
